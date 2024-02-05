0e6795fd70c85d920681d6383364661089921327
Indra Levi Manahan <indra.levi.manahan@gmail.com>
2024-02-04 21:21:06 -0500

    initial commit

---
+++ client/src/utils/auth.js
@@ -0,0 +1,127 @@
+// // use this to decode a token and get the user's information out of it
+// import decode from 'jwt-decode';
+
+// // create a new class to instantiate for a user
+// class AuthService {
+//   // get user data
+//   getProfile() {
+//     return decode(this.getToken());
+//   }
+
+//   // check if user's logged in
+//   loggedIn() {
+//     // Checks if there is a saved token and it's still valid
+//     const token = this.getToken();
+//     return !!token && !this.isTokenExpired(token); // handwaiving here
+//   }
+
+//   // check if token is expired
+//   isTokenExpired(token) {
+//     try {
+//       const decoded = decode(token);
+//       if (decoded.exp < Date.now() / 1000) {
+//         return true;
+//       } else return false;
+//     } catch (err) {
+//       return false;
+//     }
+//   }
+
+//   getToken() {
+//     // Retrieves the user token from localStorage
+//     return localStorage.getItem('id_token');
+//   }
+
+//   login(idToken) {
+//     // Saves user token to localStorage
+//     localStorage.setItem('id_token', idToken);
+//     window.location.assign('/');
+//   }
+
+//   logout() {
+//     // Clear user token and profile data from localStorage
+//     localStorage.removeItem('id_token');
+//     // this will reload the page and reset the state of the application
+//     window.location.assign('/');
+//   }
+// }
+
+// export default new AuthService();
+
+
+import decode from 'jwt-decode';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql', // Adjust the URI according to your GraphQL server endpoint
+});
+
+const authLink = setContext((_, { headers }) => {
+  // Retrieve the token from localStorage
+  const token = localStorage.getItem('id_token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const apolloClient = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+class AuthService {
+  // get user data
+  getProfile() {
+    return decode(this.getToken());
+  }
+
+  // check if user's logged in
+  async loggedIn() {
+    try {
+      // Checks if there is a saved token and it's still valid
+      const response = await apolloClient.query({
+        query: YOUR_ME_QUERY, // Replace with your actual ME query
+      });
+
+      return !!response.data.me;
+    } catch (error) {
+      console.error('Error checking authentication:', error);
+      return false;
+    }
+  }
+
+  // check if token is expired
+  isTokenExpired(token) {
+    try {
+      const decoded = decode(token);
+      return decoded.exp < Date.now() / 1000;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  getToken() {
+    // Retrieves the user token from localStorage
+    return localStorage.getItem('id_token');
+  }
+
+  login(idToken) {
+    // Saves user token to localStorage
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
+  }
+
+  logout() {
+    // Clear user token and profile data from localStorage
+    localStorage.removeItem('id_token');
+    // this will reload the page and reset the state of the application
+    window.location.assign('/');
+  }
+}
+
+export default new AuthService();
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,82 @@
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    // Resolver for getting a single user by either their id or their username
+    getSingleUser: async (_, { user, params }, context) => {
+      const foundUser = await User.findOne({
+        $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
+      });
+
+      if (!foundUser) {
+        throw new Error('Cannot find a user with this id!');
+      }
+
+      return foundUser;
+    },
+  },
+  Mutation: {
+    // Resolver for creating a user, signing a token, and sending it back
+    createUser: async (_, { body }, context) => {
+      const user = await User.create(body);
+
+      if (!user) {
+        throw new Error('Something is wrong!');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    
+    // Resolver for logging in a user, signing a token, and sending it back
+    login: async (_, { body }, context) => {
+      const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
+
+      if (!user) {
+        throw new Error("Can't find this user");
+      }
+
+      const correctPw = await user.isCorrectPassword(body.password);
+
+      if (!correctPw) {
+        throw new Error('Wrong password!');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    
+    // Resolver for saving a book to a user's `savedBooks` field
+    saveBook: async (_, { user, body }, context) => {
+      try {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: user._id },
+          { $addToSet: { savedBooks: body } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+      } catch (err) {
+        console.log(err);
+        throw new Error('Failed to save the book.');
+      }
+    },
+
+    // Resolver for removing a book from `savedBooks`
+    deleteBook: async (_, { user, params }, context) => {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: user._id },
+        { $pull: { savedBooks: { bookId: params.bookId } } },
+        { new: true }
+      );
+
+      if (!updatedUser) {
+        throw new Error("Couldn't find user with this id!");
+      }
+
+      return updatedUser;
+    },
+  },
+};
+
+module.exports = resolvers;
