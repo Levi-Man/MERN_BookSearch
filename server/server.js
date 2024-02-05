b9ce26c1b35102ce31c22e3d35006422756f6018
Indra Levi Manahan <indra.levi.manahan@gmail.com>
2024-02-04 23:30:24 -0500

    Working on the sign up functionality

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
@@ -19,7 +19,12 @@ const server = new ApolloServer({
   },
 });
 
-server.applyMiddleware({ app });
+async function startApolloServer() {
+  await server.start();
+  server.applyMiddleware({ app, path: '/graphql' });
+}
+
+startApolloServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
